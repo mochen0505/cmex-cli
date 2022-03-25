798eaad5e3c25cb3b3fa6d70b4c42bd397e9fbde
mochen0505 <mochen0505@hotmail.com>
2022-03-25 18:19:54 +0800

    feat: 批量test

--- bin/cmex-test.js
+++ bin/cmex-test.js
@@ -1,12 +1,11 @@
 #!/usr/bin/env node
 
 const program = require('commander')
-const { resolve } = require('path')
 const chalk = require('chalk')
 const { exec } = require('shelljs')
 const figlet = require('figlet')
 const gradient = require('gradient-string')
-const { PROJECT_PATH } = require('../scripts/constants.js')
+const getTestDirectory = require('../utils/getDirectory')
 
 program
   .usage('[options]')
@@ -27,14 +26,16 @@ if (args.h || args.help) {
 
 const scope = args.s || args.scope
 
-const dir = `${resolve(PROJECT_PATH, './packages')}/${scope}/__tests__/index.test.js`
+const dir = getTestDirectory(scope)
 
-exec(`jest ${dir}`, () => {
-  const msg = 'Congrats!'
-  figlet(msg, (err, data) => {
-    console.log(gradient.pastel.multiline(data))
+for(let value of Object.values(dir)) {
+  exec(`jest ${value}`, () => {
+    const msg = 'Congrats!'
+    figlet(msg, (err, data) => {
+      console.log(gradient.pastel.multiline(data))
+    })
   })
-})
+}
 
 
 
